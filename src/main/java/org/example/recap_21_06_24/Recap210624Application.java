90c5f33305e5d84c4e23d1cb0e08135df50b78f6
ChrizDope <incubusid@googlemail.com>
2024-06-21 14:20:28 +0200

    Initial commit

---
+++ src/main/java/org/example/recap_21_06_24/Recap210624Application.java
@@ -0,0 +1,13 @@
+package org.example.recap_21_06_24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recap210624Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recap210624Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/recap_21_06_24/Recap210624ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.recap_21_06_24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recap210624ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
